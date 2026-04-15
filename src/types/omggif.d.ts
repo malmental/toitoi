76202f43c8d02b5c12a8b361cf02addea6025da0
malmental <orlandorojasoyarzun@gmail.com>
2026-04-15 22:49:03 +0200

    feat: add core ASCII converter and CLI setup

--- src/types/omggif.d.ts
+++ src/types/omggif.d.ts
@@ -12,5 +12,36 @@ declare module "omggif" {
     height: number;
   }
 
-  export { GifDecoder };
+  class GifReader {
+    constructor(buffer: Buffer);
+    numFrames(): number;
+    width: number;
+    height: number;
+    frameInfo(index: number): {
+      disposal: number;
+      delay: number;
+    };
+    decodeAndBlitFrameRGBA(index: number, pixels: Uint8Array): void;
+  }
+
+  interface GifWriterOptions {
+    loop?: number;
+    palette?: number[];
+    background?: number;
+  }
+
+  interface FrameOptions {
+    palette?: number[];
+    delay?: number;
+    disposal?: number;
+    transparent?: number;
+  }
+
+  class GifWriter {
+    constructor(buf: ArrayBuffer, width: number, height: number, options?: GifWriterOptions);
+    addFrame(x: number, y: number, width: number, height: number, indices: number[], options?: FrameOptions): void;
+    end(): number;
+  }
+
+  export { GifDecoder, GifReader, GifWriter };
 }
